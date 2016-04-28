b46c507e2ed3f1ca25f565dcc652579afa10ee75
ShilpaNarendra <rshilpa27@gamil.com>
2016-04-28 16:38:05 +0530

    added edit features

--- APM.WebAPI/Models/Product.cs
+++ APM.WebAPI/Models/Product.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,13 +8,9 @@ namespace APM.WebAPI.Models
     public class Product
     {
         public string Description { get; set; }
-       // [Range(100,1000,ErrorMessage="Price should be in range of 100 to 1000")]
         public decimal Price { get; set; }
         public string ProductCode { get; set; }
         public int ProductId { get; set; }
-        [Required(ErrorMessage="Product Name is required",AllowEmptyStrings=false)]
-        [MinLength(5,ErrorMessage="Product Name min length is 5 characters")]
-        [MaxLength(7,ErrorMessage="Product Name max length is 7 characters")]
         public string ProductName { get; set; }
         public DateTime ReleaseDate { get; set; }
 
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -8,6 +8,7 @@ using APM.WebAPI.Models;
 using System.Web.Http.Cors;
 using System.Web.Http.OData;
 using System.Web.Http.OData.Query;
+using System.Web.Http.Description;
 //using System.Web.OData;
 //using System.Web.Http.Cors;   
 
@@ -24,47 +25,134 @@ namespace APM.WebAPI.Controllers
        //    var productRepo = new ProductRepository();
        //    return productRepo.Retrieve();
        //}
-     [EnableQuery(AllowedQueryOptions =
-    AllowedQueryOptions.OrderBy | AllowedQueryOptions.Filter)]
-       public IQueryable<Product> Get()
+    // [EnableQuery(AllowedQueryOptions =
+    //AllowedQueryOptions.OrderBy | AllowedQueryOptions.Filter)]
+    //   public IQueryable<Product> Get()
+    //   {
+    //       var productRepo = new ProductRepository();
+    //       return productRepo.Retrieve().AsQueryable();
+    //   }
+
+       [EnableQuery(AllowedQueryOptions =
+        AllowedQueryOptions.OrderBy | AllowedQueryOptions.Filter)]
+       [ResponseType(typeof(Product))]
+       public IHttpActionResult Get()
        {
+           try
+           {
+               var productRepo = new ProductRepository();
+               return Ok(productRepo.Retrieve().AsQueryable());
+           }
+           catch(Exception ex)
+           {
+               return InternalServerError(ex);
+           }
+       }
+        //public IEnumerable<Product> Get(string search)
+        //{
+        //    var productRepo = new ProductRepository();
+        //    var product = productRepo.Retrieve();
+        //    return product.Where(p => p.ProductCode.Contains(search));
+        //}
+       [ResponseType(typeof(Product))]
+       public IHttpActionResult Get(string search)
+       {
+           try
+           { 
            var productRepo = new ProductRepository();
-           return productRepo.Retrieve().AsQueryable();
+           var product = productRepo.Retrieve();
+           return Ok(product.Where(p => p.ProductCode.Contains(search)));
+           }
+           catch (Exception ex)
+           {
+               return InternalServerError(ex);
+           }
        }
 
-        public IEnumerable<Product> Get(string search)
-        {
-            var productRepo = new ProductRepository();
-            var product = productRepo.Retrieve();
-            return product.Where(p => p.ProductCode.Contains(search));
-        }
-
         // GET api/products/5
-        public Product Get(int id)
-        {
-            var productRepo = new ProductRepository();
-            var products = productRepo.Retrieve();
-            if (id > 0)
-                return products.FirstOrDefault(p => p.ProductId == id);
-            else
-            {
-                var product = productRepo.Create();
-                return product;
-            }
-        }   
+       [ResponseType(typeof(Product))]
+       public IHttpActionResult Get(int id)
+       {
+           try
+           {
+               //throw new ArgumentNullException("this is a test message");
+               Product product;
+               var productRepo = new ProductRepository();
+               var products = productRepo.Retrieve();
+               if (id > 0)
+               {
+                   product = products.FirstOrDefault(p => p.ProductId == id);
+                   if (product == null)
+                       return NotFound();
+                   else
+                       return Ok(product);
+               }
+               else
+               {
+                   product = productRepo.Create();
+                   return Ok(product);
+               }
+           }
+           catch (Exception ex)
+           {
+               return InternalServerError(ex);
+           }
+       }   
 
         // POST api/products
-        public void Post([FromBody]Product product)
+        public IHttpActionResult Post([FromBody]Product product)
         {
-            var productRepo = new ProductRepository();
-            var newProduct= productRepo.Save(product);
+            try
+            {
+                if (product == null)
+                {
+                    return BadRequest("Product cannot be null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var productRepo = new ProductRepository();
+                var newProduct = productRepo.Save(product);
+                if (newProduct == null)
+                {
+                    return Conflict();
+                }
+                return Created<Product>(Request.RequestUri + newProduct.ProductId.ToString(), newProduct);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
         }
 
         // PUT api/products/5
-        public void Put(int id, [FromBody]Product product)
+        public IHttpActionResult Put(int id, [FromBody]Product product)
         {
-            var productRepo = new ProductRepository();
-            var newProduct = productRepo.Save(id,product);
+            try
+            {
+                if (product == null)
+                {
+                    return BadRequest("Product cannot be null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var productRepo = new ProductRepository();
+                var updatedProduct = productRepo.Save(id, product);
+                if (updatedProduct == null)
+                    return NotFound();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
         }
 
         // DELETE api/products/5
